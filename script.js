f2e90f11f1b35452e2a7ce9dced57835e3d51a14
Gornoch <dippocornelius@hotmail.com>
2022-09-19 13:51:58 +0200

    first_commit

---
+++ script.js
@@ -0,0 +1,72 @@
+var ville ="Cambrai";
+
+const hypotenuse =8;
+
+var admission =true;
+
+var nom ="Calabri";
+
+var prenom ="Léon";
+
+var age =150;
+
+
+var pomme =4;
+
+var pêches =6;
+
+var totalFruits = pomme + pêches;
+
+var miracleFruits = pomme * pêches;
+
+console.log(totalFruits);
+
+console.log(miracleFruits);
+
+
+var bonbons = 12;
+
+var enfants = 4;
+
+var nombreBonbonsParEnfants = bonbons / enfants;
+
+console.log(nombreBonbonsParEnfants);
+
+
+
+var addition = function(nombre1 , nombre2) { return nombre1 + nombre2};
+
+var résultat1 = addition(4 , 2);
+
+console.log(résultat1);
+
+
+
+var soustraction = function(nombre1 , nombre2) { return nombre1 - nombre2};
+
+var résultat2 = soustraction(4 , 2);
+
+console.log(résultat2);
+
+
+var multiplication = function(nombre1 , nombre2) { return nombre1 * nombre2};
+
+var résultat3 = multiplication(4 , 2);
+
+console.log(résultat3);
+
+
+var division = function(nombre1 , nombre2) { return nombre1 / nombre2};
+
+var résultat4 = division(4 , 2);
+
+console.log(résultat4);
+
+
+
+
+const person = firstName[,];
+
+
+
+alert("mon nom " + person);
